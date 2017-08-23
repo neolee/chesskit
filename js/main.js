f22db81f16e8387e1a0071070596273e037de3fa
Neo Lee <neo.lee@gmail.com>
2017-08-23 14:58:16 +0800

    Refactoring for better code structure

--- js/main.js
+++ js/main.js
@@ -2,9 +2,44 @@ var makeMainBoard = function (_pos, _game) {
     return makeBoard(BOARD1_ID, _pos, _game, gOrientation, gNotation, STATUS_INFO_ID, FEN_INFO_ID, PGN_INFO_ID);
 };
 
+var multi = function () {
+    gBoard2 = ChessBoard(BOARD2_ID, {
+        orientation: gOrientation,
+        showNotation: gNotation,
+        position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
+    });
+    gBoard3 = ChessBoard(BOARD3_ID, {
+        orientation: gOrientation,
+        showNotation: gNotation,
+        position: 'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1'
+    });
+
+    $('#' + BOARD2_ID).show();
+    $('#' + BOARD3_ID).show();
+};
+
+var single = function () {
+    $('#' + BOARD2_ID).hide();
+    $('#' + BOARD3_ID).hide();
+
+    if (gBoard2) gBoard2.destroy;
+    if (gBoard3) gBoard3.destroy;
+};
+
+var orientation = function (event) {
+    gOrientation = event.data.orientation;
+    [gBoard1, gBoard2, gBoard3].forEach(function (board) {
+        if (board) board.orientation(gOrientation);
+    }, this);
+};
+
+var toggleNotation = function () {
+    gNotation = !gNotation;
+};
+
 var init = function () {
     gBoard1 = ChessBoard(BOARD1_ID, {
-        showNotation: gNotation,
+        showNotation: gNotation
     });
 };
 
@@ -22,7 +57,7 @@ var pos = function () {
         a4: 'bK',
         c4: 'wK',
         d7: 'wR'
-        };
+    };
     gBoard1 = ChessBoard(BOARD1_ID, {
         orientation: gOrientation,
         showNotation: gNotation,
@@ -36,7 +71,7 @@ var freeform = function () {
     var position = {
         e1: 'wK',
         e8: 'bK'
-        };
+    };
     gBoard1 = ChessBoard(BOARD1_ID, {
         orientation: gOrientation,
         showNotation: gNotation,
@@ -47,56 +82,11 @@ var freeform = function () {
     });
 };
 
-var playWhite = function () {
-    gPlayColor = 'w';
-}
-
-var playBlack = function () {
-    gPlayColor = 'b';
-}
+var playOrder = function (event) {
+    gPlayColor = event.data.playOrder;
+};
 
 var play = function () {
     var fen = makeFEN(gBoard1.fen(), gPlayColor, 'KQkq');
     gBoard1 = makeMainBoard(fen, new Chess(fen));
-};
-
-var white = function () {
-    gOrientation = 'white';
-    if (gBoard1) gBoard1.orientation(gOrientation);
-    if (gBoard2) gBoard2.orientation(gOrientation);
-    if (gBoard3) gBoard3.orientation(gOrientation);
-};
-
-var black = function () {
-    gOrientation = 'black';
-    if (gBoard1) gBoard1.orientation(gOrientation);
-    if (gBoard2) gBoard2.orientation(gOrientation);
-    if (gBoard3) gBoard3.orientation(gOrientation);
-};
-
-var multi = function () {
-    gBoard2 = ChessBoard(BOARD2_ID, {
-        orientation: gOrientation,
-        showNotation: gNotation,
-        position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
-    });
-    $('#' + BOARD2_ID).show();
-
-    gBoard3 = ChessBoard(BOARD3_ID, {
-        orientation: gOrientation,
-        showNotation: gNotation,
-        position: 'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1'
-    });
-    $('#' + BOARD3_ID).show();
-};
-
-var single = function () {
-    if (gBoard2) gBoard2.destroy;
-    $('#' + BOARD2_ID).hide();
-    if (gBoard3) gBoard3.destroy;
-    $('#' + BOARD3_ID).hide();
-};
-
-var toggleNotation = function () {
-    gNotation = !gNotation;
 };
